92d0754cd88223b12178481887444d2fe112b983
trungx <8992414+trungx@users.noreply.github.com>
2018-09-28 14:35:09 +0700

    Create basic_component.js

---
+++ basic_component.js
@@ -0,0 +1,30 @@
+/** this type is - function component
+const Button = function(props) {
+	return (
+  	<button>GO</button>
+  );
+}**/
+//this type is - class component
+class Button extends React.Component {
+	/*constructor(props) {
+    super(props);
+    this.state = {
+    	counter: 0,
+    };
+	};*/
+  state = {counter:0};
+  handleClick = () => {
+  	this.setState({
+    	counter: this.state.counter + 1
+    })
+  } 
+	render() {
+  	return (
+      <button onClick={this.handleClick}>
+      {this.state.counter}
+      </button>
+    );
+  }
+}
+
+ReactDOM.render(<Button label="Do" />,mountNode)
